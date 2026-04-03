6779638fb7e572a78c9b42d97b3f61fde5b12039
prathmeshmurkute2 <prathmeshmurkute983@gmail.com>
2026-04-03 12:35:35 +0530

    Connected server and databse

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db_connect.js';
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port : ",PORT)
+})
+
